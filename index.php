1c5a80d1fc8fe60d2fb5714bf91aab50b947ce3b
TecAbel <abelardo96.work@gmail.com>
2019-02-14 17:43:59 -0600

    adding files

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"> 
+    <title>SASI</title>
+</head>
+<body>
+    <header>
+        <h1>Bienvenido</h1>
+    </header>
+    <main>
+        <section class="login">
+            <form action="">
+                <fieldset>
+                    <legend><h2>Ingreso</h2></legend>
+                    <label for="usuario">Usuario: </label>
+                    <input type="text" placeholder="Nombre de usuario" autocomplete="off" required>
+                    <br><br>
+                    <label for="password">Contraseña: </label>
+                    <input type="password" placeholder="Contraseña" autocomplete="off" required>
+                    <br>
+                    <input type="submit" value="Ingresar">
+                </fieldset>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
