d1024634fece55cae09fb05951c2d67e6d635065
TecAbel <abelardo96.work@gmail.com>
2019-02-14 18:07:39 -0600

    cambio de fuentes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Ubuntu" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
     <link rel="stylesheet" href="css/estilos.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"> 
@@ -12,19 +13,22 @@
 </head>
 <body>
     <header>
-        <h1>Bienvenido</h1>
+        <h1>
+            <strong>Bienvenido a SASI</strong> <br>
+            <small>Sistema Administrativo de Servicios Informáticos &reg;</small>
+        </h1>
     </header>
     <main>
         <section class="login">
             <form action="">
                 <fieldset>
                     <legend><h2>Ingreso</h2></legend>
-                    <label for="usuario">Usuario: </label>
-                    <input type="text" placeholder="Nombre de usuario" autocomplete="off" required>
+                    <label for="usuario"><strong>Usuario:</strong> </label>
+                    <input id="usuario" type="text" placeholder="Nombre de usuario" autocomplete="off" required>
                     <br><br>
-                    <label for="password">Contraseña: </label>
-                    <input type="password" placeholder="Contraseña" autocomplete="off" required>
-                    <br>
+                    <label for="password"><strong>Contraseña: </strong></label>
+                    <input id="password" type="password" placeholder="Contraseña" autocomplete="off" required>
+                    <br><br>
                     <input type="submit" value="Ingresar">
                 </fieldset>
             </form>
