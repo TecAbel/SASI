b824005ff23a731a497ad1be8183e7b19a6020a3
TecAbel <abelardo96.work@gmail.com>
2019-02-28 11:39:30 -0600

    cambio de longitud de id's

--- sql/estructura sasi.sql
+++ sql/estructura sasi.sql
@@ -1,8 +1,10 @@
 CREATE database sasi;
 use sasi;
 
+drop table servicios;
+
 create table clientes(
-	cl_gc_cli char(7) not null unique,
+	cl_gc_cli char(8) not null unique,
     primary key( cl_gc_cli),
     cl_tipo_cliente boolean not null,
     cl_nom char(30) not null,
@@ -15,7 +17,7 @@ create table clientes(
 );
 
 create table equipos(
-	eq_gc_equipo char(7) not null unique,
+	eq_gc_equipo char(8) not null unique,
     primary key(eq_gc_equipo),
     cl_gc_cli char(8) not null,
     foreign key(cl_gc_cli) references clientes(cl_gc_cli),
@@ -25,19 +27,19 @@ create table equipos(
     eq_mem_ram int(2) null,
     eq_cap_disco int(8) null,
     eq_sis_ope char(20) not null,
+    eq_sis_ope_lic char(30) null,
     eq_bits int(2) not null,
     eq_act boolean,
     eq_ant_virus char(20) null,
     eq_virus_ boolean,
     eq_office char(4) null,
     eq_lic_office char(30) null,
-    eq_notas char(50) null
+    eq_notas varchar(200) null
 );
 
-alter table equipos add column eq_sis_ope_lic char(30)  null; 
 
 create table servicios(
-	sr_gc_ser char(7) not null unique,
+	sr_gc_ser char(8) not null unique,
     primary key(sr_gc_ser),
     sr_nombre char(20) not null,
     sr_desc char(100) not null,
@@ -46,7 +48,7 @@ create table servicios(
 );
 
 create table tecnicos(
-	tc_gc_tec char(7) not null unique,
+	tc_gc_tec char(8) not null unique,
     primary key(tc_gc_tec),
     tc_nombre char(30) not null,
     tc_ap_pat char(15) not null,
@@ -56,20 +58,21 @@ create table tecnicos(
 );
 
 create table ticket(
-	tk_gc_ticket char(7) not null unique,
+	tk_gc_ticket char(8) not null unique,
+    primary key(tk_gc_ticket),
     tk_fecha date,
     tk_total double(7,2) not null
 );
 create table detalleticket(
-	dt_gc_dt char(7) not null unique,
+	dt_gc_dt char(8) not null unique,
     primary key(dt_gc_dt),
-    tc_gc_tec char(7) not null,
+    tc_gc_tec char(8) not null,
     foreign key(tc_gc_tec) references tecnicos(tc_gc_tec),
-    sr_gc_ser char(7) not null,
+    sr_gc_ser char(8) not null,
     foreign key(sr_gc_ser) references servicios(sr_gc_ser),
-    eq_gc_equipo char(7) not null,
+    eq_gc_equipo char(8) not null,
     foreign key(eq_gc_equipo) references equipos(eq_gc_equipo),
-    tk_gc_ticket char(7) not null,
+    tk_gc_ticket char(8) not null,
     foreign key(tk_gc_ticket) references ticket(tk_gc_ticket),
     dt_sub_total double(7,2) null
 );
--- sql/nuevo usuario.sql
+++ sql/nuevo usuario.sql
@@ -10,7 +10,23 @@ GRANT insert,select ON sasi.* to 'abelardo'@'localhost' ;
 create user 'abelardo'@'localhost' identified by 'An18';
 
 
-SELECT *FROM clientes;
+SELECT *FROM CLIENTES;
 #inserción de clientes
 INSERT INTO clientes (cl_gc_cli,cl_tipo_cliente,cl_nom,cl_ap_pat,cl_ap_mat,cl_tel,cl_fac_a,cl_dir,cl_rfc)
-VALUES ('CI190000','0','ABELARDO','AQUI','ARROYO','5578444332','','BENITO JUÁREZ','');
+VALUES ('CI19000','0','ABELARDO','AQUI','ARROYO','5578444332','','BENITO JUÁREZ','');
+
+#obtener el último registro
+SELECT max(cl_gc_cli) as Último_registro FROM clientes;
+
+#obtener últimos tres valores del registro
+SELECT right(max(cl_gc_cli),3) FROM clientes;
+#obtener últimos tres valores del registro y sumarle uno
+SELECT right(max(cl_gc_cli),3)+ '00' + 1 FROM clientes;
+
+select max(cl_gc_cli)+1 from clientes;
+
+INSERT INTO clientes (cl_gc_cli,cl_tipo_cliente,cl_nom,cl_ap_pat,cl_ap_mat,cl_tel,cl_fac_a,cl_dir,cl_rfc)
+VALUES ('CI196','0','ABELARDO','AQUI','ARROYO','5578444332','','BENITO JUÁREZ','');
+
+#vaciar tabla
+TRUNCATE table CLIENTES;
